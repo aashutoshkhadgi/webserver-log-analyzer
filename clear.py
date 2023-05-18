512feb8ff5922b3b32b3ac227e70604e034889d1
haha <haha@kali.haha>
2023-05-18 15:02:11 -0400

    added necessary changed and matploit for top ip in horizontal bar chart

--- clear.py
+++ clear.py
@@ -5,6 +5,6 @@ def Remove_Output(Book):
             cell.outputs = []
         if hasattr(cell, "prompt_number"):
             del cell["prompt_number"]
-Book = read(open("log-analyzer.py.ipynb"), 4)
+Book = read(open("new-log-analyzer.py.ipynb"), 4)
 Remove_Output(Book)
-write(Book, open("new-log-analyzer.py.ipynb", "w"), 4)
+write(Book, open("log-analyzer.py.ipynb", "w"), 4)
